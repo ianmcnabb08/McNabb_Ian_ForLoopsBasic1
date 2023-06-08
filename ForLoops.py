2558f06546980e642312827a36574f17a2064fa5
Ian McNabb <ianmcnabb08@gmail.com>
2023-06-08 17:12:03 -0500

    Python ForLoops

---
+++ ForLoops.py
@@ -0,0 +1,26 @@
+#1
+for i in range(151):
+    print(i)
+
+#2
+for i in range(5, 1001, 5):
+    print(i)
+
+#3
+for i in range(101):
+    if i % 5:
+        print("Coding")
+    elif i % 10:
+        print("Coding Dojo")
+    else:
+        print(i)
+
+#4
+sum = 0
+for i in range(1,500001,2):
+    sum += i
+print(i)
+
+#5
+for i in range(2018,0,-4):
+    print(i)
